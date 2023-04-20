5c3fbd4b94de2f4d6a13c7b11c99b714486a3a7d
Márta Gergely <ma.gergo3@gmail.com>
2023-04-20 19:45:52 +0200

    L8 updates
    	- health check
    	- connect error handling
    	- adding curl in docker file
    	- environment variables for netflix db and port

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-import express, { Router } from 'express'
-import methodOverride from 'method-override'
-import mongoose, { connect } from 'mongoose'
+import express, { Router } from 'express';
+import methodOverride from 'method-override';
+import mongoose, { connect } from 'mongoose';
 import restify from 'express-restify-mongoose';
-import queueRouter from './src/routes/queue.js'
-import sessionRouter from './src/routes/sessions.js'
+
+import queueRouter from './src/routes/queue.js';
+import sessionRouter from './src/routes/sessions.js';
 const { serve } = restify;
 
 import videos from './src/models/videos.js'
 import users from './src/models/users.js'
 import sessions from './src/models/sessions.js'
 
-const port = 10021;
+const port = process.env.PORT || 10021;
 
 
 const app = express()
@@ -19,7 +20,10 @@ const router = Router()
 app.use(express.json())
 app.use(methodOverride())
 
-connect('mongodb://netflix-db:27017/netflix')
+connect(`mongodb://${process.env.NETFLIX_DB}:27017/netflix`).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
 
 const serveOptions = {
   prefix: '',
@@ -30,6 +34,21 @@ const serveOptions = {
 serve(router, videos, serveOptions)
 serve(router, users, serveOptions)
 serve(router, sessions, serveOptions)
+
+router.get('/health', async (req, res) => {
+  try {
+    const collections = await mongoose.connection.db.listCollections().toArray();
+    if (collections.length > 0) {
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(500);
+    }
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+
 
 app.use(queueRouter)
 app.use(sessionRouter)
